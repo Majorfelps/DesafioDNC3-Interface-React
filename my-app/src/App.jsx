0566f3691d8e7364dd088c9df47072fe3e01297f
Majorfelps <michael.as.cruz@gmail.com>
2023-10-23 22:44:54 -0300

    Adicionando rotas e componentes

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import './global.scss'
+import React from "react";
+import { List } from "./views/List";
+
+
+
+//utilizar esse mockup como exemplo
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const db = [
+    { "id": 1, "title": "Exercicios", "description": "Ir para academia fazer exercicios", "completed": true },
+    { "id": 2, "title": "Limpar o carro", "description": "Limpar o carro inteiro, de dentro pra fora", "completed": false },
+    { "id": 3, "title": "Banho e tosa", "description": "Levar o cachorro ao pet shop", "completed": false },
+    { "id": 4, "title": "Limpar quarto", "description": "Limpar toda bagunça que está dentro do quarto", "completed": true },
+    { "id": 5, "title": "trabalhar", "description": "Chegar ao escritorio antes das 20:00", "completed": true },
+    { "id": 6, "title": "Ir ao banco", "description": "Chear ao banco antes das 10:00", "completed": false },
+    { "id": 7, "title": "Almoçar", "description": "Preparar a comida para a janta", "completed": false },
+    { "id": 8, "title": "Jogar volei", "description": "Ir a quadra para jogar volei com os amigos", "completed": true },
+    { "id": 9, "title": "Estudar programação", "description": "Entrar na plataforma dos alunos para estudar", "completed": false },
+    { "id": 10, "title": "shopping", "description": "Fazer algumas compras no shopping", "completed": true }
+  ]
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App" >
+      <List />
+    </div>
+  );
 }
 
-export default App
+export default App;
