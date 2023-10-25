50e1736f227ede5b7284297e929c1b7855aaff05
Majorfelps <michael.as.cruz@gmail.com>
2023-10-25 10:11:05 -0300

    Adicionado Navbar

---
+++ my-app/src/components/navbar/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div>
+        <div class="navbar">
+            <h1>Organização</h1>
+            <h1>Tarefas</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Nav
